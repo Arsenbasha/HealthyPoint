a8815b8c853c1bf6253c2bdd16e6a62651289aa0
leandroparedes <leandroparedes0721@gmail.com>
2021-01-13 15:57:03 +0100

    porject full

--- app/src/main/java/com/arsenbasha/healthypoint/aboutme.kt
+++ app/src/main/java/com/arsenbasha/healthypoint/aboutme.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 
@@ -13,7 +14,8 @@ class aboutme : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Sobre Nosotros"
+        (activity as AppCompatActivity?)!!.supportActionBar!!.subtitle = "Alumnos"
 
         return inflater.inflate(R.layout.fragment_aboutme, container, false)
         setHasOptionsMenu(true);
